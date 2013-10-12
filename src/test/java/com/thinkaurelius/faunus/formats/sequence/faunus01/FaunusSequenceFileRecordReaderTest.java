78524f8e83cc98e08c762257e251bbfe1b090533
John Skopis <john.skopis@causes.com>
2013-10-12 12:57:04 -0700

    Change hadoop to CDH4-mr1
    This commit swaps out the hadoop1 jar for the CDH4 hadoop2-mr1 jar.
    
    We also pull in a custom version of cassandra-all that uses CDH4.

--- src/test/java/com/thinkaurelius/faunus/formats/sequence/faunus01/FaunusSequenceFileRecordReaderTest.java
+++ src/test/java/com/thinkaurelius/faunus/formats/sequence/faunus01/FaunusSequenceFileRecordReaderTest.java
@@ -6,7 +6,7 @@ import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.mapred.TaskAttemptID;
-import org.apache.hadoop.mapreduce.TaskAttemptContext;
+import org.apache.hadoop.mapreduce.task.TaskAttemptContextImpl;
 import org.apache.hadoop.mapreduce.lib.input.FileSplit;
 
 import java.util.HashMap;
@@ -20,7 +20,7 @@ public class FaunusSequenceFileRecordReaderTest extends BaseTest {
     public void testRecordReader() throws Exception {
         FaunusSequenceFileRecordReader reader = new FaunusSequenceFileRecordReader();
         reader.initialize(new FileSplit(new Path(FaunusSequenceFileRecordReaderTest.class.getResource("graph-of-the-gods-faunus01.seq").toURI()), 0, Long.MAX_VALUE, new String[]{}),
-                new TaskAttemptContext(new Configuration(), new TaskAttemptID()));
+                new TaskAttemptContextImpl(new Configuration(), new TaskAttemptID()));
 
         int counter = 0;
         final Map<Long, FaunusVertex> graph = new HashMap<Long, FaunusVertex>();
